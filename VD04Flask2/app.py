271e90723423f0250ee4143a86e4e274e98811ca
rrd23 <123895790+rrd23@users.noreply.github.com>
2024-08-15 19:12:15 +0300

    create app.py

---
+++ VD04Flask2/app.py
@@ -0,0 +1,23 @@
+# /app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Маршрут для главной страницы
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Маршрут для страницы блога
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+# Маршрут для страницы контактов
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
