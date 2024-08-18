c8f1971371292b0f75cd5f3fbbd736e39b87498c
rrd23 <123895790+rrd23@users.noreply.github.com>
2024-08-18 16:09:40 +0300

    lesson VD06

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+#создаёт экземпляр класса Flask (переменную app)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+# Импортируем маршруты:
+from app import routes
+
